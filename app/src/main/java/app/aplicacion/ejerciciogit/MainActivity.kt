28c40b4f7f1cca52d6712437132b2488a710db08
Darwin Chamba <darwinchambaa@gmail.com>
2024-06-11 09:41:13 -0500

    segundo print

--- app/src/main/java/app/aplicacion/ejerciciogit/MainActivity.kt
+++ app/src/main/java/app/aplicacion/ejerciciogit/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("esta es una nueva version")
+        print("segundo print ")
     }
 }
