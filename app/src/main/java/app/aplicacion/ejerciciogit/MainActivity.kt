7d7b1cc1a5159b69fe45a79bf8637b1adf0aad22
Darwin Chamba <darwinchambaa@gmail.com>
2024-06-11 10:54:58 -0500

    eliminando prints en la rama proyecto

--- app/src/main/java/app/aplicacion/ejerciciogit/MainActivity.kt
+++ app/src/main/java/app/aplicacion/ejerciciogit/MainActivity.kt
@@ -7,11 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("esta es una nueva version")
-        print("segundo print ")
-        print("tercer print")
-        print("comentario de stash")
 
-        print("comentario en la rama proyecto")
     }
 }
