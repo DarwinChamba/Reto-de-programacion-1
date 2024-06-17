53b25fc818e45bab62e1075e10ed94a631f67ba7
Darwin Chamba <darwinchambaa@gmail.com>
2024-06-17 10:37:41 -0500

    agregando ultimas modificaciones

---
+++ app/src/main/java/app/aplicacion/ejerciciogit/ui/AddCartActivity.kt
@@ -0,0 +1,12 @@
+package app.aplicacion.ejerciciogit.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.aplicacion.ejerciciogit.R
+
+class AddCartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_cart)
+    }
+}
